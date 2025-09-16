60b3b13dc4f67ffc3ba1cb6e6bbd77da8f6e1e3b
br7forlife <diazpachecobrayan@gmail.com>
2025-09-16 13:51:03 -0500

    create to model folder

---
+++ SpringData/01-basic-crud/src/main/java/com/archives/__basic_crud/models/UserEntity.java
@@ -0,0 +1,22 @@
+package com.archives.__basic_crud.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String occupation;
+    private String mail;
+    private Long cellphone;
+}
