bdf476dee3de0bbcb1c4fa581929e9ff050020cb
br7forlife <diazpachecobrayan@gmail.com>
2025-09-22 12:19:04 -0500

    fix and some changes

--- SpringData/01-basic-crud/src/main/java/com/archives/__basic_crud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.archives.__basic_crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.archives.__basic_crud.models.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer>{
-    
-}
