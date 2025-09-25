bfa5b2763523c2e1eca6833da4907a100c72d79d
br7forlife <diazpachecobrayan@gmail.com>
2025-09-25 10:01:21 -0500

    fix joincolumn and inverseJoinColumn

--- SpringData/01-basic-crud/src/main/java/com/archives/__basic_crud/models/OrdersEntity.java
+++ SpringData/01-basic-crud/src/main/java/com/archives/__basic_crud/models/OrdersEntity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,10 +23,10 @@ public class OrdersEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ManyToMany
-    @JoinTable(name = "Customers_orders", joinColumns = @JoinColumn(name = "customer_id"), inverseJoinColumns = @JoinColumn(name = "orders_id"))
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "Customers_orders", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "customer_id"))
     List<CustomersEntity> customers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
     List<ProductsEntity> products = new ArrayList<>();
 }
